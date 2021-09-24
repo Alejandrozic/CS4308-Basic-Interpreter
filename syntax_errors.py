c789f4f008ab25a56533f26b1de5bb5262392655
Alejandrozic <alejandrozic@gmail.com>
2021-09-24 13:41:27 -0400

    Create syntax_errors.py

---
+++ syntax_errors.py
@@ -0,0 +1,19 @@
+"""
+    File contains the various SyntaxError that are
+    checked by the Lexical Analyzer.
+"""
+
+
+class SyntaxErrorMissingEndingQuote(Exception):
+    """ Start Quote is missing an end Quote """
+    pass
+
+
+class SyntaxErrorInvalidInteger(Exception):
+    """ Integer is Invalid """
+    pass
+
+
+class SyntaxErrorInvalidCharacter(Exception):
+    """ Character was not defined in our grammar and thus is not supported """
+    pass
