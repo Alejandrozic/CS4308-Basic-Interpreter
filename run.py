639a8742cf5637c3c49c1db783133aad0450865d
AZ <alejandrozic@gmail.com>
2021-09-24 14:14:19 -0400

    adding argument options

--- run.py
+++ run.py
@@ -2,9 +2,20 @@
 Tested with Python 3.6.4
 """
 
+import sys
 import interpreter.lexical_analyzer
 
 
-file = 'source_files/example2.bas'
-lexical_analyzer = interpreter.lexical_analyzer.LexicalAnalyzer(file)
-lexical_analyzer.run()
+def main():
+    lexical_analyzer = interpreter.lexical_analyzer.LexicalAnalyzer(file)
+    lexical_analyzer.run()
+
+
+if __name__ == '__main__':
+    try:
+        # Try to get the file as a argument
+        file = sys.argv[1]
+    except IndexError:
+        # If no arguments are passed, use the default
+        file = 'source_files/example1.bas'
+    main()
